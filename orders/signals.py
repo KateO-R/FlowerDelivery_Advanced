fc0516deae7e9b521fa05583ee39810ee2e45cde
KateO-R <veg2710@gmail.com>
2025-02-06 14:09:28 +0500

    renewed code for tg bot

--- orders/signals.py
+++ orders/signals.py
@@ -1,10 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from orders.models import Order
-from bot.bot import send_order_notification  # Импортируем функцию отправки уведомлений
+# from bot.bot import send_order_notification  # Импортируем функцию отправки уведомлений
+def notify_order_created(order):
+    from bot.bot import send_order_notification  # Перемещаем импорт внутрь функции
+    send_order_notification(order)
 
 @receiver(post_save, sender=Order)
 def notify_order_status_change(sender, instance, **kwargs):
     """Отправка уведомления при изменении статуса заказа."""
     if instance.status_has_changed:  # Проверяем, изменился ли статус
+        from bot.bot import send_order_notification  # Перемещаем импорт внутрь функции
         send_order_notification(instance)
